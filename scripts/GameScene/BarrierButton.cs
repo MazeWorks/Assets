0560fb0b845d63937b9e898309df4977ac21ea3b
daimon <32012820+MazeWorks@users.noreply.github.com>
2017-09-17 23:29:34 +0900

    daimonの中身出しただけ

---
+++ scripts/GameScene/BarrierButton.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BarrierButton : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void ButtonPush()
+    {
+        GameObject g = GameObject.FindGameObjectWithTag("Player");
+        g.SendMessage("GetItem", 1);
+    }
+}
