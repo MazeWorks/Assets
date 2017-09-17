b79c39d2a82f514b5883435161200ba38044d3db
daimon <32012820+MazeWorks@users.noreply.github.com>
2017-09-17 22:34:41 +0900

    とりあえずassetだけ上げたやで

---
+++ daimon/Scripts/GameScene/Barrier.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Barrier : MonoBehaviour {
+
+    // プレイヤーのオブジェクト
+    GameObject player;
+
+    // アクティブか
+    public bool isActive;
+
+	// Use this for initialization
+	void Start () {
+        gameObject.SetActive(false);
+        player = GameObject.FindGameObjectWithTag("Player");
+        isActive = false;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        transform.position = player.transform.position;
+	}
+
+    public void active()
+    {
+        gameObject.SetActive(true);
+        isActive = true;
+    }
+
+    public void inactive()
+    {
+        gameObject.SetActive(false);
+        isActive = false;
+    }
+}
